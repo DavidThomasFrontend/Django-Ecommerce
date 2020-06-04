589d74ffbc248506d204d992902555097146a780
Vileblood <david.thomas1986@gmail.com>
2020-06-04 20:34:07 +0000

    added sorting and product counts

--- products/views.py
+++ products/views.py
@@ -11,28 +11,25 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
-    sort =  None
+    sort = None
     direction = None
 
-
     if request.GET:
-
-        if request.GET:
-            if 'sort' in request.GET:   #check whether sort is in request.get
-                sortkey = request.GET['sort'] #if it is, we set to equal both sort which be none at this point
-                sort = sortkey
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
             if sortkey == 'name':
-                sortkey = 'lower_name'   #rename sortkey to 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))# annotate current list of products with new field
-
-        if 'direction' in request.GET:
-            direction = request.GET['direction']
-            if direction == 'desc':         #check whether direction is descending order in order to decide the order
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-        if 'category' in request.GET:
             
+        if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
@@ -52,6 +49,7 @@ def all_products(request):
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
