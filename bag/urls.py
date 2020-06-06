f20e4acc2a32867ef586bce2ca8165e75f42d4f0
Vileblood <david.thomas1986@gmail.com>
2020-06-06 16:09:36 +0000

    added add to bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
 from . import views
-urlpatterns = [
-    path('', views.view_bag, name='view_bag')
 
+urlpatterns = [
+    path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
 ]
