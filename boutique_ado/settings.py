36e16e53a7007be802de2db341843ac2d1bdf7fc
Vileblood <david.thomas1986@gmail.com>
2020-06-15 22:26:22 +0000

    added checkout template and crispy forms

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    #Other
+
+    'crispy-forms'
     
 ]
 
@@ -60,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutique_ado.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,6 +83,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
