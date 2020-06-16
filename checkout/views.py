abdb685052a5952a805b8f6eee2ea84ed5c60374
Vileblood <david.thomas1986@gmail.com>
2020-06-16 20:57:15 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51GulB3LxmzpAngyb9vhzJIcb69S9pcJmT3LYyHhdbygUutPLrSZNqQywCvdP0LATbVl8LvWYm2eSYLarIPXMGpIU00yek5ceO9',
+        'client_secret': 'test client secret'
 
         }
 
