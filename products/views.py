b4f0fbf4e4e4d15ae3e008cca4a07e85c3312820
Vileblood <david.thomas1986@gmail.com>
2020-06-29 21:02:39 +0000

    deployment part 1

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-rom django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
