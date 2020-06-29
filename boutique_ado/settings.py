db2e4196334d70faa215bf4e308f6607830a611c
Vileblood <david.thomas1986@gmail.com>
2020-06-29 21:17:52 +0000

    deployment part 2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dthomas-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
