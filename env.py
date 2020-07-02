7d3338185b7a41bdce9e9970c08cec16a527562b
Vileblood <david.thomas1986@gmail.com>
2020-07-02 19:50:03 +0000

    fixed untracked files

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("STRIPE_PUBLIC_KEY", 'pk_test_51GulB3LxmzpAngybEgKPLf06k5Dxx24TZOAy4L0ZjZAI7ytSnexCLmCyeQxn9FHA0dP6mjXLYoKuPvvdCpfoBfGv00sHpWDAGF')
-os.environ.setdefault("STRIPE_SECRET_KEY", 'sk_test_51GulB3LxmzpAngybbtOkow72I2pDiwzy1cIR3X1VF0A1L1JtSlS4eeAwc1C4aDg1Xe98obbvgNN827nJXWKylcc600sUxaRemL')
-os.environ.setdefault("STRIPE_WH_SECRET", 'whsec_23O4rr3IINGNSBx0qcXimqTjbRcgQpDr')
-
-os.environ.setdefault("SECRET_KEY", 'h!q%ljtiy7llsg6-@h6&r3x8_46pd&cngd8f1_65x+pm^eap33')
